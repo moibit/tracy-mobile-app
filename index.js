29972756b472c43fc9b26caa60ae08e8911f6069
oshobajpai <osho1278@gmail.com>
2020-03-27 02:27:54 +0530

    push-notification commit

--- index.js
+++ index.js
@@ -7,4 +7,5 @@ import App from './App';
 import { name as appName } from './app.json';
 import { gestureHandlerRootHOC } from 'react-native-gesture-handler';
 
+
 AppRegistry.registerComponent(appName, () => gestureHandlerRootHOC(App));
